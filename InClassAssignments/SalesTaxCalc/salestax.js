90c80b4cf270b4cac70761c37bf43078d17e1619
gtanky <89743429+gtanky@users.noreply.github.com>
2022-09-15 12:26:16 -0500

    Create salestax.js

---
+++ InClassAssignments/SalesTaxCalc/salestax.js
@@ -0,0 +1,32 @@
+var $ = function(id) {
+    return document.getElementById(id);
+};
+
+var calculate = function() {
+    var subtotal = parseFloat($("subtotal").value);
+    var taxRate = parseFloat($("tax_rate").value);
+    var shippingCharges = parseFloat($("shipping_charges").value);
+    var salesTax;
+    var total;
+    
+    /*if (isNaN(subtotal) || isNaN(taxRate) || isNaN(shippingCharges)) {
+        alert("Please enter numbers");
+        return false;
+    }*/
+    
+    salesTax = subtotal * (taxRate / 100);
+    
+    total = subtotal + salesTax + shippingCharges;
+    
+    $("sales_tax").value = "$" + salesTax.toFixed(2);
+    
+    $("total").value = "$" + total.toFixed(2);
+    
+};
+
+window.onload = function(){
+    $("subtotal").value = "";
+    $("tax_rate").value = "";
+    $("shipping_charges").value = "";
+    $("calculate").onclick = calculate;
+};
