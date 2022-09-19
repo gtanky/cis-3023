3de568633074274a7d16c864e438fffde54b51b5
gtanky <89743429+gtanky@users.noreply.github.com>
2022-09-19 18:02:21 -0500

    Update monthlyPayment.js

--- monthlyPayment.js
+++ monthlyPayment.js
@@ -1,36 +1,28 @@
 var $ = function(id) {
-    return document.getElementById(id);
-}
+	return document.getElementById(id);
+};
 
- var click = function() {
-    var fullName = $("full_name").value;
-    var initialLoanAmount = $("initial_loan").value;
-    var numMonths = $("num_months").value;
-    var annualInterest = $("annual_interest").value;
-    var monthlyInterestRate = annualInterest / 1200;
-    var monthlyPayment = $("monthly_payment").value;
-    
-    if (fullName == "") {        
-        $("monthly_payment").value = "";
-    }
-    
-    else if (initialLoanAmount < 0 || monthsOfLoan < 0 || annualInterest < 0 || annualInterest > 100) {    
-        $("monthly_payment").value = "";
-    }
-    
-    else {
-        monthlyPayment = initialLoanAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, monthsOfLoan * -1)));
-            
-        $("monthly_payment").value = "$" + monthlyPayment.toFixed(2);
+var calc = function() {
+	var initial = parseFloat($("initial_loan").value);
+	var months = parseFloat($("num_months").value);
+	var interest = parseFloat($("annual_interest").value);
+	var payment = $("annual_payment").value;
+	//var total = $("total").value;
 
-        return false;
-    }
- 
-}
+	payment = initial * ((interest/(12*100))/(1 - ((1 + (interest/(12*100))) ** (-1 * months))))
+	
+	$("payment_monthly").value = "$" + payment.toFixed(2);
+	
+    return false;
+	
 
-window.onload = function() {
-    $("initial_loan_amount").value = "";
-    $("months_of_loan").value = "";
-    $("annual_interest").value = "";
-    $("calculate").onclick = click;
+};
+
+ window.onload = function () 
+{
+    $("loan_initial").value = "";
+    $("num_months").value = "";
+	$("interest_annual").value = "";
+	$("payment_monthly").value = "";
+
 }
