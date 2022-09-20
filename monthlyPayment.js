3a4d6e79947b5b450d9b0e054117208862053b15
gtanky <89743429+gtanky@users.noreply.github.com>
2022-09-19 22:45:07 -0500

    Update monthlyPayment.js

--- monthlyPayment.js
+++ monthlyPayment.js
@@ -6,7 +6,7 @@ var calc = function() {
 	var initial = parseFloat($("initial_loan").value);
 	var months = parseFloat($("num_months").value);
 	var interest = parseFloat($("annual_interest").value);
-	var payment = $("annual_payment").value;
+	var payment = $("payment_monthly").value;
 
 	payment = initial * ((interest/(12*100))/(1 - ((1 + (interest/(12*100))) ** (-1 * months))))
 	
@@ -18,9 +18,9 @@ var calc = function() {
 
  window.onload = function () 
 {
-    $("loan_initial").value = "";
+    $("initial_loan").value = "";
     $("num_months").value = "";
-	$("interest_annual").value = "";
+	$("annual_interest").value = "";
 	$("payment_monthly").value = "";
     //$("calc").onclick = calc;
 }
