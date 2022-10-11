2255a76f12af793c629a057921a0985f82369657
gtanky <89743429+gtanky@users.noreply.github.com>
2022-10-11 00:37:35 -0500

    Update myCalculator.js

--- Assignments/myCalculator.js
+++ Assignments/myCalculator.js
@@ -9,46 +9,45 @@ for (; breakValue != "EXIT";) {
 			if (!isNaN(menuOption) && menuOption > 0 && menuOption < 8) {
 				return (menuOption);
 			}
-			alert("Please enter a number 1-7");
+			alert("Error! Please enter a number between 1-7");
 		}//for
-	}//menu function
+	}
 
 	var addition = function () {
 		var num1 = numVerify("Enter the first number here");
 		var num2 = numVerify("Enter the second number here");
 		alert(num1 + num2);
-	}//addition function
+	}
 
 	var subtraction = function () {
 		var num1 = numVerify("Enter the first number here");
 		var num2 = numVerify("Enter the second number here that will be subtracted from the first");
 		alert(num1 - num2);
-	}//subtraction function
+	}
 
 	var multiplication = function () {
 		var num1 = numVerify("Enter the first number here");
 		var num2 = numVerify("Enter the second number here that will be multiplied with the first");
 		alert(num1 * num2);
-	}//multiplication function
+	}
 
 	var division = function () {
 		var num1 = numVerify("Enter the first number / numerator here");
 		var num2 = numVerify("Enter the second number / denominator here that the first number will be divided by");
 		alert(num1 / num2);
-	}//division function
+	}
 
 	var exponential = function () {
 		var num1 = numVerify("Enter the base number here");
 		var num2 = numVerify("Enter the exponent here");
 		alert(num1 ** num2);
-	}//exponential function
+	}
 
 	var average = function () {
 		var num1 = 0;
 		var num2 = 0;
 		var meanCount = 0;
 		for (; ;) {
-			//alert(num2)
 			num1 = prompt("Enter a number to add to the average here or enter '***' to end");
 			if (!isNaN(parseFloat(num1))) {
 				num2 += parseFloat(num1);
@@ -62,12 +61,12 @@ for (; breakValue != "EXIT";) {
 			}
 		}//for
 		alert(num2 / meanCount);
-	}//average function
+	}
 
 	var exitProgram = function () {
 		alert("Thank you for using my Calculator, have a nice day!");
 		breakValue = "EXIT";
-	}//exit function
+	}
 
 	var numVerify = function (promptText) {
 		for (; ;) {
@@ -77,28 +76,28 @@ for (; breakValue != "EXIT";) {
 			}
 			alert("Please enter a number");
 		}//for
-	}//verification function
+	}
 
 	switch (menu() - 1) {
-		case 0://Add
+		case 0:
 			addition();
 			break;
-		case 1://Subtract
+		case 1:
 			subtraction();
 			break;
-		case 2://Multiply
+		case 2:
 			multiplication();
 			break;
-		case 3://Divide
+		case 3:
 			division();
 			break;
-		case 4://Exponent
+		case 4:
 			exponential();
 			break;
-		case 5://Mean
+		case 5:
 			average();
 			break;
-		case 6://Quit
+		case 6:
 			exitProgram();
 			break;
 	}//switch
