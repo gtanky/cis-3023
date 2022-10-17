4550cafade7208010c2f67f45fc82c200e2f32e4
gtanky <89743429+gtanky@users.noreply.github.com>
2022-10-17 16:10:53 -0500

    Update GarrettTankersley.js

--- Assignments/GarrettTankersley.js
+++ Assignments/GarrettTankersley.js
@@ -1,17 +1,19 @@
 var $ = function (id) {
 	return document.getElementById(id);
 };
-for (; ;) {
-	vowelCount = 0;
-	var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
-	if (usrInput == "***") {
-		alert("Thank you for using Vowel Counter, have a nice day!");
-		break
-	}
-	for (var i = 0; i < usrInput.length; i++) {
-		if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
-			vowelCount++;
+var vowelCounter = function () {
+	for (; ;) {
+		vowelCount = 0;
+		var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
+		if (usrInput == "***") {
+			alert("Thank you for using Vowel Counter, have a nice day!");
+			break
 		}
+		for (var i = 0; i < usrInput.length; i++) {
+			if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
+				vowelCount++;
+			}
+		}
+		document.getElementById("myHeader").innerHTML = vowelCount;
 	}
-	alert(vowelCount);
 }
