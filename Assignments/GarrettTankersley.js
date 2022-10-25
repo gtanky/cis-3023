a63bdff874263178ab9638833ab52eb67c7ed184
gtanky <89743429+gtanky@users.noreply.github.com>
2022-10-24 20:42:20 -0500

    Update GarrettTankersley.js

--- Assignments/GarrettTankersley.js
+++ Assignments/GarrettTankersley.js
@@ -7,7 +7,7 @@ var vowelCounter = function () {
 		var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
 		if (usrInput == "***") {
 			alert("Thank you for using Vowel Counter, have a nice day!");
-			break;
+			clear;
 		}
 		for (var i = 0; i < usrInput.length; i++) {
 			if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
