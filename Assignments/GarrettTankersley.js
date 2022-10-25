03b030e1844e7a31c782d06dbfeb411ddaf5d6d1
gtanky <89743429+gtanky@users.noreply.github.com>
2022-10-24 20:56:40 -0500

    Update GarrettTankersley.js

--- Assignments/GarrettTankersley.js
+++ Assignments/GarrettTankersley.js
@@ -1,34 +1,19 @@
-function $(id){
-    return document.getElementById(id);
-}
-
-function countVowels_click() {
-    var userWord = "";
-    var userVowels = "";
-    
-        for(;!isNaN(userWord);) {
-            userWord = prompt("Enter a string or *** to exit");
-        }
-        
-        userWord = userWord.toLowerCase();
-        if (userWord == "***") {
-            alert("Thanks for using the vowel counter!");
-            clear();
-        }
-        userVowels = countVowels1(userWord);
-    
-    $("resultBox").value = userVowels;
-    countVowels_click();
- }
-    
-
-function countVowels1(userWord) {
-    var insideVowels = userWord.match(/[aeiou]/gi);
-    return insideVowels.length;
-}
-
-
-
-function clear() {
-    $("resultBox").value = "";
+var $ = function (id) {
+	return document.getElementById(id);
+};
+var vowelCounter = function () {
+	for (; ;) {
+		vowelCount = 0;
+		var usrInput = prompt("Please enter a string here to count the total vowels or enter '***' to end");
+		if (usrInput == "***") {
+			alert("Thank you for using Vowel Counter, have a nice day!");
+			break;
+		}
+		for (var i = 0; i < usrInput.length; i++) {
+			if (usrInput.charAt(i).toLowerCase() == "a" || usrInput.charAt(i).toLowerCase() == "e" || usrInput.charAt(i).toLowerCase() == "i" || usrInput.charAt(i).toLowerCase() == "o" || usrInput.charAt(i).toLowerCase() == "u") {
+				vowelCount++;
+			}
+		}
+		document.getElementById("myHeader").innerHTML = vowelCount;
+	}
 }
